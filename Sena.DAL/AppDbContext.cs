a63ae17b87eb67c94d7ab812ec73b962a6fc9b21
lvgutierrez02 <71912902+lvgutierrez02@users.noreply.github.com>
2022-04-26 12:11:26 -0500

    hoy

--- Sena.DAL/AppDbContext.cs
+++ Sena.DAL/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Sena.Models.Entities;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Sena.DAL
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
